ea822adcadbe508544a42cba13761293ff9f5ccb
anasauce <anasauceda1843@gmail.com>
2017-06-11 15:44:57 -0700

    adds styling to landing page

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express()
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
@@ -20,5 +21,5 @@ app.use(passport.session())
 
 app.use('/', index)
 app.use('/users', users)
- 
+
 export default app
